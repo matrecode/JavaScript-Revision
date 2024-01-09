4041ae0ad89dfb9ff076f08d1d1e9c7050a1b136
matrecode <akshaymatrecode@gmail.com>
2024-01-09 17:10:02 +0530

    Date chapter Done

--- console-dt-string-date/index.js
+++ console-dt-string-date/index.js
@@ -233,3 +233,88 @@ console.log("harr dee harr dee harr".indexOf("dee", 10))
 console.log("abc".repeat(3))
 
 
+// DATE 
+
+var now = new Date()
+console.log(typeof(now))
+console.log(typeof(now.toString()))
+
+var one = new Date() // dd mm dd yyyy hr:m:s TZ (TZname)
+var february = new Date(2024,1)
+console.log(february)
+
+// Convert to a string format
+var date1 = new Date()
+console.log(date1)
+console.log(date1.toTimeString()) // hh:m:s Time Zone TIMEZONE name 11:27:46 GMT+0530 (India Standard Time)
+
+// convert to date string
+console.log(date1.toDateString()) // Tue Jan 09 2024
+// To UTC string
+console.log(date1.toUTCString()) // Tue, 09 Jan 2024 06:00:02 GMT
+// to ISO string (International organization for standarization)
+console.log(date1.toISOString()) // 2024-01-09T06:00:32.039Z
+// to GMT string (Greenwich Mean Time)
+console.log(date1.toGMTString()) // Tue, 09 Jan 2024 06:01:57 GMT
+// to locale date string
+console.log(date1.toLocaleDateString()) // 1/9/2024
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+console.log(date1.toLocaleDateString([], options))
+
+
+// Creating a date from UTC (Co-ordinated Universal Time)
+
+function formatDate(dayOfWeek, day, month, year){
+        var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        var months = ["Jan", "Feb", "Mar", "Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+        return `${daysOfWeek[dayOfWeek]} ${months[month]} ${day} ${year}`
+}
+
+var birthday = new Date(Date.UTC(1999, 8, 27))
+console.log("Akshay was born on : " + formatDate(birthday.getUTCDay(), birthday.getUTCDate(), birthday.getUTCMonth(), birthday.getUTCFullYear()))
+
+// changing the date object
+
+var date = new Date();
+date.setUTCFullYear(2000,0,31);
+date.setUTCHours(24,0,0,0);
+console.log(date)
+
+// Increment a Date Object
+
+var checkoutDate = new Date();
+console.log(checkoutDate)
+checkoutDate.setHours(checkoutDate.getHours() + 1)
+console.log(checkoutDate)
+
+
+// Adding Work Days
+// if you are working on the task and you have to set the deadline then this calculator will come into the picture
+function addWorkDays(startDate, days){
+        var dayOfWeek = startDate.getDay()
+        var daysToAdd = days
+        if(dayOfWeek == 0){
+                daysToAdd + 1
+        }
+
+        if(dayOfWeek + daysToAdd >= 6){
+                // substarct days in current working week from work days
+                var remainingWorkDays = daysToAdd - (5-dayOfWeek)
+                //  Add current working weeks weekend
+                daysToAdd += 2
+                if (remainingWorkDays > 5){
+                        daysToAdd += 2 * Math.floor(remainingWorkDays / 5)
+                        if(remainingWorkDays % 5 == 0){
+                                daysToAdd -= 2
+                        }
+                } 
+        }
+        
+        startDate.setDate(startDate.getDate() + daysToAdd)
+        return startDate
+}
+
+console.log(addWorkDays(new Date(), 10))
+
+
+
